b6a3f26d38897e71489bda28a0e2d4f174a52d48
Peteous <erik.peterman@und.edu>
2017-03-28 14:41:52 -0500

    Comment code

--- demo-alert.js
+++ demo-alert.js
@@ -1,1 +1,6 @@
-alert("This is my first ever JavaScript!")
+/*
+I am in chapter 1 of a learn Javascript course.
+This is very rudimentary code.
+*/
+
+alert("This is my first ever JavaScript!") // creates an alert box
