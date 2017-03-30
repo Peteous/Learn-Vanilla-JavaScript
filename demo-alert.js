f62e8bf336d279ac88147f26ecd45ec66110525d
Peteous <erik.peterman@und.edu>
2017-03-30 14:09:30 -0500

    Variables introduction

--- demo-alert.js
+++ demo-alert.js
@@ -2,5 +2,9 @@
 I am in chapter 1 of a learn Javascript course.
 This is very rudimentary code.
 */
+var example = 100
+var print = "This is my first ever JavaScript!"
 
-alert("This is my first ever JavaScript!") // creates an alert box
+alert(print) // creates an alert box
+
+document.write(example)
