a979d5d8846b8791ac8acf7a1e0d97afdbea764a
Peteous <erik.peterman@und.edu>
2017-03-30 14:18:22 -0500

    Added conditional operator introduction

--- demo-alert.js
+++ demo-alert.js
@@ -2,9 +2,18 @@
 I am in chapter 1 of a learn Javascript course.
 This is very rudimentary code.
 */
-var example = 100
-var print = "This is my first ever JavaScript!"
+var example = 100;
 
-alert(print) // creates an alert box
+var print = "This is my first ever JavaScript!";
 
-document.write(example)
+alert(print); // creates an alert box
+
+document.write(example);
+
+example++;
+
+alert(example == 100);
+
+++example;
+var conditionalOperator = (example <= 101) ? "Not Incremented": "Incremented";
+document.write(conditionalOperator);
