bf217beb837a8d857c3a97dbe8b7ec78eef63b27
Peteous <erik.peterman@und.edu>
2017-03-31 08:45:41 -0500

    Switch and for loop introduction

--- demo-alert.js
+++ demo-alert.js
@@ -6,14 +6,12 @@ var example = 100;
 
 var print = "This is my first ever JavaScript!";
 
-if(example != Null && print != Null){
+if(example != 0 && print != ""){
 	alert(print); // creates an alert box
 }
 else {
 	document.write("Nards");
 }
-
-document.write(example);
 
 example++;
 
@@ -22,3 +20,19 @@ alert(example == 100);
 ++example;
 var conditionalOperator = (example <= 101) ? "Not Incremented": "Incremented";
 document.write(conditionalOperator);
+
+switch(example){
+	case 100:
+		document.write("100");
+		break;
+	case 101:
+		document.write("101");
+		break;
+	default:
+		example++
+		break;
+}
+
+for (i=1; i<=5; i++) {
+	document.write(example+i + "<br />");
+}
